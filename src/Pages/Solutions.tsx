b74b642bf212366eaa1695e8313752774228eb88
Siddhartha Banerjee <59687592+Siddhartha097@users.noreply.github.com>
2024-02-29 18:13:02 +0530

    first commit

--- src/Pages/Solutions.tsx
+++ src/Pages/Solutions.tsx
@@ -4,7 +4,7 @@ import { Separator } from "@/components/ui/separator"
 
 const Solutions = () => {
   return (
-    <main className="relative p-14 w-full h-full space-y-14">   
+    <main className="relative p-20 w-full h-full space-y-14">   
 
       <section className="relative w-[90vw] h-[90vh] max-md:h-[50vh] max-md:w-full rounded-md flex flex-col justify-center p-10 max-md:p-6 space-y-6 bg-cover bg-[url('https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
          <div className="space-y-5 bg-slate-600/20 relative text-white p-10 max-md:p-0">
@@ -63,31 +63,38 @@ const Solutions = () => {
 
       <Separator className="my-12" />
 
-      <section>
-        <div>
+      <section className="flex items-center justify-between space-x-8 ">
+        <div className="w-1/2 space-y-8">
 
-          <div>
-            <h1 className="text-5xl">Discover the Cost-Effective Solution For Your Business</h1>
-            <p>
+          <div className="space-y-6">
+            <h1 className="text-4xl font-bold">Discover the Cost-Effective Solution For Your Business</h1>
+            <p className="font-medium text-neutral-500">
               Our third solution offers a cost-effective way to meet your business needs. With its innovative features and competitive pricing. you can save both time and money.
             </p>
           </div>
 
-          <div>
+          <div className="flex items-center justify-between space-x-7">
             <Box 
-              label="Benefits:"
+              label="1. Benefits:"
               iconDisabled
+              className="w-1/2 space-y-5"
+              desc="Streamline operations and reduced overhead costs with our efficient solution"
             />
+            <Separator orientation="vertical" />
             <Box 
-              label="Features:"
+              label="2. Features:"
               iconDisabled
+              className="w-1/2 space-y-5"
+              desc="Advanced analytics to track and make data-driven decisions."
             />
           </div>
 
         </div>
 
-        <div>
-          <img src="" alt="" />
+        <Separator className="my-10" orientation="vertical" />
+
+        <div className="w-1/2">
+          <img className="rounded-lg shadow-md" src="https://images.unsplash.com/photo-1474631245212-32dc3c8310c6?q=80&w=1924&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="sol" />
         </div>
 
       </section>
