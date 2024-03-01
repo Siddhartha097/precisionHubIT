91eee3475fcf7de0ed41ac76be771a4d7a9936ef
SouvikDasG <123242028+SouvikDasG@users.noreply.github.com>
2024-03-01 15:17:19 +0530

    minute changes

--- src/Pages/Learn.tsx
+++ src/Pages/Learn.tsx
@@ -2,27 +2,37 @@ import React from 'react'
 
 const Learn = () => {
   return (
-    <div className='flex justify-center'>
+    <main className='LearnMore-container'>
+        <div className='flex justify-center'>
 
-    <div className="flex pt-56 justify-center h-screen bg-white px-20 w-3/5">
-        <h1 className="text-6xl font-bold text-gray-800 mb-8">
-             Powerful Software Solutions for Your Business
-        </h1>
-    </div>
-    <div className=' pt-56 pr-20'>
-        <p className="text-xl text-gray-700 mb-8 ">
-             Transform your business with our innovative software solutions. Increase efficiency, streamline processes, and drive growth.
-        </p>
-        <div>
-            <button className="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 ">
-            Learn More
-            </button>
-            <button className="bg-white hover:bg-blue-700 text-black font-bold border-2 py-2 px-4 ml-4">
-             Sign Up
-            </button>
+            <div className="flex pt-56 justify-center h-screen bg-white px-20 w-3/5">
+                <h1 className="text-6xl font-bold text-gray-800 mb-8">
+                    Powerful Software Solutions for Your Business
+                </h1>
+            </div>
+            <div className=' pt-56 pr-20'>
+                <p className="text-xl text-gray-700 mb-8 ">
+                    Transform your business with our innovative software solutions. Increase efficiency, streamline processes, and drive growth.
+                </p>
+                <div>
+                    <button className="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 ">
+                    Learn More
+                    </button>
+                    <button className="bg-white hover:bg-blue-700 text-black font-bold border-2 py-2 px-4 ml-4">
+                    Sign Up
+                    </button>
+                </div>
+            </div>
         </div>
-    </div>
-  </div>
+        <div className='learn-page2 flex justify-between'>
+            <div>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+            </div>
+            <div><img src="" alt="" className='bg-black'/>hi</div>
+        </div>
+    </main>
   )
 }
 
