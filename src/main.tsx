57268dce5d7e48d29cd1218671238c08cc65c6c5
Siddhartha Banerjee <59687592+Siddhartha097@users.noreply.github.com>
2024-03-01 14:10:02 +0530

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,16 @@ import Navbar from './components/Navbar.tsx'
 import Footer from './components/Footer.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Solutions from './Pages/Solutions.tsx'
+import Researches from './Pages/Researches.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path='/solutions' element={<Solutions />} />
         <Route path='/' element={<App />} />
+        <Route path='/solutions' element={<Solutions />} />
+        <Route path='/researches' element={<Researches />}/>
       </Routes>
       <Footer />
     </BrowserRouter>
