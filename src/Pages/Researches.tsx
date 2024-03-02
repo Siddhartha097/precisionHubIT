ef584ddac8de4ac4a986da6107343cb6b7f67dd3
Siddhartha Banerjee <59687592+Siddhartha097@users.noreply.github.com>
2024-03-02 17:39:16 +0530

    first commit

--- src/Pages/Researches.tsx
+++ src/Pages/Researches.tsx
@@ -17,14 +17,37 @@ const Researches = () => {
                 </div>
             </section>
 
-            <Separator className="my-14" />
+            <Separator className="my-14 " />
 
-            <section className="flex items-center justify-between space-x-7">
-                <div>
-                    
+            <section className="flex items-center justify-center space-x-8 *:w-1/2">
+
+                <div className="w-1/2 space-y-8 ">
+                    <h1 className="font-bold text-4xl">Simplify Your Research Process with Our Long Heading Feature</h1>
+                    <p className="font-medium text-neutral-600 max-md:text-sm">Our software is designed to streamline the research process, making it easier and more efficient for researchers.</p>
+                    <div className="flex *:w-1/2 items-center justify-between space-x-7 *:space-y-5">
+                        <div>
+                            <h1 className="text-4xl font-bold">
+                                50%
+                            </h1>
+                            <p className="font-medium text-neutral-500 text-sm">Save time and effort with our powerful research tools.</p>
+                        </div>
+                        <div>
+                            <h1 className="text-4xl font-bold">
+                                50%
+                            </h1>
+                            <p className="font-medium text-neutral-500 text-sm">Access valuable insights and make informed decisions.</p>
+                        </div>
+                    </div>
                 </div>
-                <div></div>
+
+                <div className="w-1/2">
+                    <img className="rounded-md scale-90 hover:scale-95 transition-all" src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                </div>
+
             </section>  
+
+            {/* <Separator className="my-14 " /> */}
+
         </main>
     )
 }
