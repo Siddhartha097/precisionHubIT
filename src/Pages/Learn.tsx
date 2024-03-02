e56d418724e45748af4a8cd109a41b29cc1c248d
SouvikDasG <123242028+SouvikDasG@users.noreply.github.com>
2024-03-02 10:43:20 +0530

    c

--- src/Pages/Learn.tsx
+++ src/Pages/Learn.tsx
@@ -24,21 +24,60 @@ const Learn = () => {
                 </div>
             </div>
         </div>
-        <div className='learn-page2 grid grid-cols-2 px-20'>
-            <div className='px-20'>
-                <h1 className='text-6xl font-bold'>Revolutionize WorkFlow With Our Software</h1>
-                <div className='py-10 font-semibold text-gray-500'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis accusamus eaque nulla alias fugit tempora voluptas quae maxime, error velit eos minima dolores voluptatibus aliquam odio, tempore consequatur, ullam eum?</div>
-                <div className='flex'>
-                    <Box label='Powerful Solutions' desc='lorem dak ajdiw hsjda hasdhaw hawduud hadwuhd hwah h u uwhadu' className='*:h-16'></Box>
-                    <Box label='Powerful Solutions' desc='lorem dak ajdiw hsjda hasdhaw hawduud hadwuhd hwah h u uwhadu' className='*:h-16'></Box>
-                </div>
-            </div>
-            <div><img src="https://media.istockphoto.com/id/865215930/vector/abstract-colorful-geometric-square-border-pattern-on-white-background.jpg?s=2048x2048&w=is&k=20&c=8s7O24vxGlHlzwROjyrmIdAfwjne-Sbs6zUcCh8Czc4=" alt="" className=' px-20'/></div>
-            <ul className='flex px-20'>
-                <button className='border-2 p-3 mr-2'>Learn More</button>
-                <button>Sign Up </button>
-            </ul>
+        <div className='learn-page2'>
+                    <div className=' grid grid-cols-2'>
+                        <div className='px-20'>
+                            <h1 className='text-6xl font-bold'>Revolutionize WorkFlow With Our Software</h1>
+                            <div className='py-10 font-semibold text-gray-500'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis accusamus eaque nulla alias fugit tempora voluptas quae maxime, error velit eos minima dolores voluptatibus aliquam odio, tempore consequatur, ullam eum?</div>
+                            <div className='flex'>
+                                <Box label='Powerful Solutions' desc='lorem dak ajdiw hsjda hasdhaw hawduud hadwuhd hwah h u uwhadu' className='*:h-16'></Box>
+                                <Box label='Powerful Solutions' desc='lorem dak ajdiw hsjda hasdhaw hawduud hadwuhd hwah h u uwhadu' className='*:h-16'></Box>
+                            </div>
+                        </div>
+                        <div><img src="https://media.istockphoto.com/id/865215930/vector/abstract-colorful-geometric-square-border-pattern-on-white-background.jpg?s=2048x2048&w=is&k=20&c=8s7O24vxGlHlzwROjyrmIdAfwjne-Sbs6zUcCh8Czc4=" alt="" className=' px-20 h-full'/></div>
+                        <ul className='flex px-20'>
+                            <button className='border-2 p-3 mr-2'>Learn More</button>
+                            <button>Sign Up </button>
+                        </ul>
+                    </div>
+
+
+                    <div className='grid grid-cols-3 px-20 py-40'>
+                        <section>
+                            <Box label='Powerful Solutions' desc='lorem dak ajdiw hsjda hasdhaw hawduud hadwuhd hwah h u uwhadu' className='*:h-16' titleSize='text-5xl'></Box>
+                            <ul className='flex'>
+                            <button className='border-2 p-3 mr-2 '>Learn More</button>
+                            <button>Sign Up </button>
+                        </ul>
+                        </section>
+                        <section>
+                            <Box label='Powerful Solutions' desc='lorem dak ajdiw hsjda hasdhaw hawduud hadwuhd hwah h u uwhadu' className='*:h-16'></Box>
+                            <ul className='flex'>
+                            <button className='border-2 p-3 mr-2'>Learn More</button>
+                            <button>Sign Up </button>
+                        </ul>
+                        </section>
+                        <section>
+                            <Box label='Powerful Solutions' desc='lorem dak ajdiw hsjda hasdhaw hawduud hadwuhd hwah h u uwhadu' className='*:h-16'></Box>
+                            <ul className='flex'>
+                            <button className='border-2 p-3 mr-2'>Learn More</button>
+                            <button>Sign Up </button>
+                        </ul>
+                        </section>
+                    </div>
+
+
+                    <div className='flex px-20'>
+                        <section className='text-6xl w-2/4 font-bold'>
+                            Discover the power of Our Software
+                        </section>
+                        <section className='w-2/4 text-xl'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, magni eum natus quas corrupti quae soluta cupiditate adipisci, quod sint repellat tempore id hic et voluptate! Odio magnam tenetur quas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore temporibus asperiores dolorum nemo enim, in, iure sequi est eligendi minima id placeat repellat. Ipsa, iusto iste non cum commodi enim!
+                        </section>
+                    </div>
         </div>
+
     </main>
   )
 }
