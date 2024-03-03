eb64d30d85f3075997385f2f28a70819340a90ad
Siddhartha Banerjee <59687592+Siddhartha097@users.noreply.github.com>
2024-03-03 16:02:23 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from "react-hook-form";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "./components/ui/form";
 import { Input } from "./components/ui/input";
-import { Carousel, CarouselContent, CarouselItem} from "./components/ui/carousel";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious} from "./components/ui/carousel";
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "./components/ui/collapsible";
 import { useState } from "react";
 
@@ -47,7 +47,7 @@ const FAQs = [
     
   },
   {
-    title: 'What are te benefits?',
+    title: 'What are the benefits?',
     content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, aspernatur animi sit dolores labore dolor odio ad iste qui vero.',
 
   },
@@ -57,7 +57,7 @@ const FAQs = [
 
   },
   {
-    title: 'How can I get  support?',
+    title: 'How can I get support?',
     content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, aspernatur animi sit dolores labore dolor odio ad iste qui vero.',
 
   },
@@ -120,116 +120,117 @@ const App = () => {
 
   return (
 
-    <main className="p-14 space-y-16">
+    <main className="p-14 max-md:px-8 space-y-16 max-md:space-y-10">
 
         {/* 1st */}
         <section className="flex flex-col items-center justify-center space-y-6">
-          <h1 className="text-7xl font-bold text-center">Transforming businesses <div className="p-2" /> with innovative software <div className="p-2" /> solutions</h1>
-          <p className="text-center">Welcome to our company, where we create cutting-edge software solutions to help businesses <div className="p-1" /> thrive in the digital age.</p>
+          <h1 className="text-7xl max-md:text-4xl font-bold text-center">Transforming businesses with innovative software solutions</h1>
+          <p className="text-center max-md:text-sm">Welcome to our company, where we create cutting-edge software solutions to help businesses <div className="p-1" /> thrive in the digital age.</p>
           <div className="space-x-6">
             <Button className="rounded-none border border-black hover:bg-green-600 hover:border-green-600 hover:text-white" variant={'outline'}>Learn More</Button>
           </div>
         </section>
 
-        <Separator className="my-10" />
+        <Separator className="my-10 max-md:my-6" />
 
         {/* 2nd */}
-        <section className="w-full px-16 py-10 flex items-center justify-center">
+        <section className="w-full scale-90 flex items-center justify-center">
           <img className="object-contain shadow-md rounded-lg" src="https://images.unsplash.com/photo-1604328698692-f76ea9498e76?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="hero" />
         </section>
 
-        <Separator className="my-10" />
+        <Separator className="my-10 max-md:my-6" />
 
         {/* 3rd */}
-        <section className="flex items-center justify-between space-x-8">
+        <section className="flex max-md:flex-col-reverse max-md:space-x-0 max-md:space-y-7 items-center justify-between space-x-8">
 
           {/* non-image */}
-          <div className="space-y-8 w-1/2">
+          <div className="space-y-8">
 
             {/* title and description */}
-            <Label htmlFor="text">Innovative</Label>
-            <h1 className="text-4xl font-bold tracking-wide">Transforming Ideas into Powerful Solutions</h1>
-            <p className="text-sm font-medium text-neutral-500 tracking-wide">
+            <Label className="hidden md:block" htmlFor="text">Innovative</Label>
+            <h1 className="text-4xl max-md:text-2xl font-bold tracking-wide max-md:text-center">Transforming Ideas into Powerful Solutions</h1>
+            <p className="text-sm font-medium text-neutral-500 tracking-wide max-md:text-xs max-md:text-center">
               We provide cutting-edge software solutions that drive innovation and empower businesses to succeed. Our team of experts combines creativity and technical expertise to deliver exceptional results.
             </p>
 
             {/* boxes */}
-            <div className="flex items-center justify-between space-x-7">
+            <div className="flex items-center justify-between space-x-5">
               <Box 
-                className="space-y-5"
+                className="space-y-4 max-md:flex max-md:text-center flex-col items-center justify-center"
                 label="Experience"
                 desc="Over 10 years of industry experience delivering top-notch software solutions."
               />
               <Box 
-                className="space-y-5"
+                className="space-y-4 max-md:flex max-md:text-center flex-col items-center justify-center"
                 label="Quality"
                 desc="We are committed to delivering high-quality software solutions that exceed client expectations."
               />
             </div>
 
             {/* buttons */}
-            <div className="space-x-4">
-              <Button className="bg-transparent text-black font-semibold border-2 hover:bg-green-600 hover:border-green-600 hover:text-white border-black rounded-none p-5">Learn More</Button>
+            <div className="space-x-4 flex items-center max-md:justify-center">
+              <Link to={'/learn'}><Button className="bg-transparent text-black font-semibold border-2 hover:bg-green-600 hover:border-green-600 hover:text-white border-black rounded-none p-5">Learn More</Button></Link>
               <Link to={'#'} className="p-6 rounded-none border-transparent hover:text-green-600 font-semibold">Contact Us </Link>
             </div>
 
           </div>
 
           {/* image */}
-          <div className="w-1/2">
+          <div className="w-1/2 max-md:w-full">
             <img className="rounded-md shadow-md scale-95 hover:scale-100 transition-all " src="https://images.unsplash.com/photo-1708614896043-9ff9d03d641d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="hero" />
           </div>
 
         </section>
 
-        <Separator className="my-10" />
+        <Separator className="my-10 max-md:my-6" />
 
         {/* 4th */}
-        <section className="flex items-center justify-between gap-20">
+        <section className="flex max-md:flex-col items-center justify-between gap-10">
 
           {/* Image */}
-          <div className="basis-1/2">
-            <img className="rounded-md shadow-md scale-95 transition-all hover:scale-100" src="https://images.unsplash.com/photo-1708556863286-16a9ada29871?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="feature" />
+          <div className=" md:w-1/2">
+            <img className="rounded-md shadow-md scale-90 transition-all hover:scale-95" src="https://images.unsplash.com/photo-1708556863286-16a9ada29871?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="feature" />
           </div>
 
           {/* Features */}
-          <div className="basis-1/2 space-y-10">
+          <div className="md:w-1/2 space-y-10">
             {Features.map(feature =>(
-              <div className="space-y-5" key={feature.label}>
-                <h1 className="text-3xl font-bold">{feature.label}</h1>
-                <p className="">{feature.para}</p>
+              <div className="space-y-5 max-md:text-center" key={feature.label}>
+                <h1 className="text-3xl max-md:text-xl font-bold">{feature.label}</h1>
+                <p className="max-md:text-sm">{feature.para}</p>
               </div>
             ))}
           </div>
 
         </section>
 
-        <Separator className="my-10" />
+        <Separator className="my-10 max-md:my-6" />
 
         {/* 5th */}
-        <section className="flex items-center justify-around gap-16">
+        <section className="flex max-md:flex-col-reverse items-center justify-around gap-10">
 
           {/* non-image */}
-          <div className="w-1/3 space-y-5">
-            <h1 className="text-3xl font-bold">
+          <div className="md:w-1/3 space-y-5 max-md:text-center">
+            <h1 className="text-3xl font-bold max-md:text-2xl">
               Unlock the full potential of your business with our powerful software solutions.
             </h1>
-            <p className="text-neutral-500 font-medium">
+            <hr />
+            <p className="text-neutral-600 font-medium max-md:text-sm">
               Our software solutions are designed to streamline your operations, increase efficiency, and drive growth. Experience the difference today.
             </p>
           </div>
 
           {/* image */}
-          <div className="w-1/2">
+          <div className="md:w-1/2">
             <img className="rounded-lg shadow-md scale-95 hover:scale-100 transition-all" src="https://images.unsplash.com/photo-1708704974484-3ca56db9384f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="room" />
           </div>
 
         </section>
 
-        <Separator className="my-10" />
+        <Separator className="my-10 max-md:my-6" />
 
         {/* 6th */}
-        <section className="flex items-center justify-between gap-10">
+        <section className="flex max-md:flex-col items-center justify-between gap-10">
           <Box 
             label="Streamline your workflow
             with our software"
@@ -262,10 +263,10 @@ const App = () => {
           />
         </section>
 
-        <Separator className="my-10" />
+        <Separator className="my-10 max-md:my-6" />
 
         {/* 7th */}
-        <section className="flex items-center justify-between space-x-8">
+        <section className="flex max-md:flex-col max-md:*:w-full items-center justify-between max-md:space-y-6 space-x-8 max-md:space-x-0">
 
           {/* image */}
           <div className="w-1/2">
@@ -273,25 +274,26 @@ const App = () => {
           </div>
 
           {/* non-image */}
-          <div className="space-y-8">
+          <div className="space-y-8 max-md:text-center">
             {Supports.map(support => (
-              <div className="space-y-6">
-                <h1 className="text-3xl font-bold">{support.label}</h1>
-                <p className="text-sm font-medium text-neutral-500">{support.desc}</p>
+              <div className="space-y-5">
+                <h1 className="text-3xl font-bold max-md:text-2xl">{support.label}</h1>
+                <hr />
+                <p className="text-sm font-medium text-neutral-500 max-md:text-xs">{support.desc}</p>
               </div>
             ))}
           </div>
         </section>
 
-        <Separator className="my-10" />
+        <Separator className="my-10 max-md:my-6" />
 
         {/* 8th */}
-        <section className="flex items-center justify-between space-x-7">
+        <section className="flex items-center max-md:*:w-full justify-between space-x-7 max-md:space-x-0 max-md:space-y-6 max-md:flex-col">
 
           {/* left */}
-          <div className="w-1/2 space-y-4">
+          <div className="w-1/2 space-y-4 max-md:text-center">
             <Label htmlFor="text">Innovative</Label>
-            <h1 className="text-5xl font-bold">
+            <h1 className="text-5xl max-md:text-3xl font-bold">
               Transforming the way businesses operate with cutting-edge solutions
             </h1>
           </div>
@@ -299,30 +301,30 @@ const App = () => {
           
 
           {/* right */}
-          <div className="w-1/2 space-y-8">
-            <h3 className="font-medium">
+          <div className="w-1/2 space-y-8 max-md:text-center">
+            <h3 className="font-medium max-md:text-sm text-neutral-600">
               With over a decade of experience, we have successfully delivered exceptional software solutions to clients worldwide. Our team of experts is dedicated to providing innovative and reliable products that meet the unique needs of each business.
             </h3>
 
-            <div className="flex justify-between items-center">
+            <div className="flex justify-between items-center max-md:space-x-5">
 
-              <div className="space-y-2">
-                <h1 className="text-5xl font-bold">50%</h1>
-                <p className="text-neutral-600 font-medium">
+              <div className="space-y-3">
+                <h1 className="text-5xl font-bold max-md:text-4xl">50%</h1>
+                <p className="text-neutral-600 font-medium max-md:text-xs">
                   Increase in productivity for our clients
                 </p>
               </div>
-              <Separator orientation="vertical" />
-              <div className="space-y-2">
-                <h1 className="text-5xl font-bold">50%</h1>
-                <p className="text-neutral-600 font-medium">
+              <Separator orientation="vertical" className="max-md:hidden" />
+              <div className="space-y-3">
+                <h1 className="text-5xl font-bold max-md:text-4xl">50%</h1>
+                <p className="text-neutral-600 font-medium max-md:text-xs">
                   Reduction in operational costs for our clients
                 </p>
               </div>
 
             </div>
 
-            <div className="font-medium space-x-5">
+            <div className="font-medium space-x-5 flex items-center max-md:justify-evenly">
               <Button className="border-2 border-black rounded-none p-5 hover:bg-green-600 hover:border-green-600 hover:text-neutral-200 transition-all" variant={'outline'}>Learn More</Button>
               <Link className="hover:text-green-600" to={'#'}>Contact Us</Link>
             </div>
@@ -330,36 +332,38 @@ const App = () => {
 
         </section>
 
-        <Separator className="my-10" />
+        <Separator className="my-10 max-md:my-6" />
 
         {/* 9th */}
         <section>
           <img className="rounded-lg scale-90 shadow-md transition-all" src="https://images.unsplash.com/photo-1541746972996-4e0b0f43e02a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
         </section> 
 
-        <Separator className="my-10" />
+        <Separator className="my-10 max-md:my-6" />
 
         {/* 10th  */}
         <section className="space-y-8">
 
           <div className="space-y-5 text-center">
             <Label htmlFor="text">Innovative</Label>
-            <h1 className="text-5xl font-bold">Locations</h1>
-            <p className="font-medium text-neutral-500">We have offices worldwide to serve you better.</p>
+            <h1 className="text-5xl font-bold max-md:text-4xl">Locations</h1>
+            <p className="font-medium text-neutral-500 max-md:text-sm">We have offices worldwide to serve you better.</p>
           </div>
 
-          <div className="p-10">
+          <div className="p-10 max-md:p-6">
             <Carousel opts={{loop: true}} className="flex items-center justify-center">
+            <CarouselNext className="md:hidden" />
               <CarouselContent>
                 {Locations.map(location => (
-                  <CarouselItem key={location.label} className="basis-1/4 pl-4 space-y-6 text-center">
+                  <CarouselItem key={location.label} className="basis-1/4 max-md:basis-full pl-4 space-y-6 text-center">
                     <img className="rounded-lg shadow-md" src={location.imgSrc} alt={location.label} />
                     <h1 className="text-4xl font-bold">{location.label}</h1>
                     <p className="font-medium text-neutral-500">{location.address}</p>
-                    <Link className="font-medium hover:text-purple-600 transition-all" to={'#'}>Get directions</Link>
+                    <Link className="font-medium hover:text-purple-600 transition-all underline" to={'#'}>Get directions</Link>
                   </CarouselItem>
                 ))}
               </CarouselContent>
+              <CarouselPrevious className="md:hidden" />
             </Carousel>
           </div>
 
@@ -368,16 +372,16 @@ const App = () => {
         {/* 11th */}
         <section className="flex flex-col items-center space-y-6 justify-between">
           <div className="space-y-6 text-center">
-            <h1 className="text-5xl font-bold">Frquently Asked Questions</h1>
-            <p className="font-medium text-neutral-500">Find answers to common questions about our company and solutions.</p>
+            <h1 className="text-5xl font-bold max-md:text-3xl">Frquently Asked Questions</h1>
+            <p className="font-medium text-neutral-500 max-md:text-sm">Find answers to common questions about our company and solutions.</p>
           </div>
 
           <div className="space-y-6 transition-all">
             {FAQs.map(faq => (
-              <Collapsible className="transition-all space-y-5 relative w-[70vw] border-2 p-6 border-black">
+              <Collapsible className="transition-all space-y-5 relative w-[70vw] border-2 p-6 max-md:p-4 border-black">
                 <div className="flex items-center justify-between relative ">
-                  <h3 className="text-xl font-semibold">{faq.title}</h3>
-                  <CollapsibleTrigger onClick={changeSign} className={`absolute right-0 transition-all text-3xl font-bold`}>+</CollapsibleTrigger>                 
+                  <h3 className="text-xl font-semibold max-md:text-lg">{faq.title}</h3>
+                  <CollapsibleTrigger onClick={changeSign} className={`absolute right-0 transition-all text-3xl font-bold ${!sign ? "" : ''}`}>+</CollapsibleTrigger>                 
                 </div>
                 <CollapsibleContent className="font-medium transition-all text-neutral-500">
                   {faq.content}
