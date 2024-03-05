d3a3a407e923ecd13561f3f215c59bdca68ae1d6
Siddhartha Banerjee <59687592+Siddhartha097@users.noreply.github.com>
2024-03-05 18:02:57 +0530

    first commit

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -8,6 +8,7 @@ import { Separator } from "@/components/ui/separator";
 import { techVid } from "@/images";
 
 import { BsBoxFill } from "react-icons/bs";
+import { FaStar } from "react-icons/fa";
 
 
 const WorkFlow = [
@@ -316,7 +317,28 @@ const About = () => {
 
             <Separator className="my-10" />
 
-            
+            <section className="flex max-md:flex-col items-center justify-between gap-10">
+
+                <div className="flex items-center justify-center md:w-2/3">
+                    <img src="https://images.unsplash.com/photo-1516321318423-f06f85e504b3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="rounded-lg shadow-md scale-90 hover:scale-95 transition-all" alt="" />
+                </div>
+
+                <div className="flex flex-col items-center justify-center gap-5 md:w-1/3 space-y-4">
+                    <div className="flex space-x-2 mb-5 items-center justify-center">
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                    </div>
+                    <h1 className="font-semibold">Our experience with the software has been nothing short of amazing. It has greatly improved our productivity and efficiency.</h1>
+                    <div className="text-center flex items-center justify-center space-x-8">
+                      <p className="text-gray-700 max-md:text-sm"> <b>Rick Dawson, CEO,</b><br /> XYZ Corp</p>
+                      <p className="text-3xl  font-bold">|</p>
+                      <p className="font-bold">WorkFlow</p>
+                    </div>
+                </div>
+            </section>
 
         </main>
     )
