80cc9a1eebb9349c18ba8ffa9d7726b4af5352d1
Siddhartha Banerjee <59687592+Siddhartha097@users.noreply.github.com>
2024-03-08 16:41:15 +0530

    first commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,115 +1,111 @@
-import { FaFacebook, FaInstagram, FaLinkedin, FaYoutube, } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaLinkedin, FaYoutube } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { Separator } from "./ui/separator";
 import { Link } from "react-router-dom";
 import { Logo } from "@/images";
 
 const Services = [
-    {
-        label: 'About Us',
-        link: '/about',
-    },
-    {
-        label: 'Services',
-        link: '#',
-    },
-    {
-        label: 'Contact',
-        link: '/contact',
-    },
-    {
-        label: 'FAQ',
-        link: '#',
-    },
-    {
-        label: 'Blog',
-        link: '#',
-    },
+  {
+    label: "About Us",
+    link: "/about",
+  },
+  {
+    label: "Services",
+    link: "#",
+  },
+  {
+    label: "Contact",
+    link: "/contact",
+  },
+  {
+    label: "FAQ",
+    link: "#",
+  },
+  {
+    label: "Blog",
+    link: "#",
+  },
 ];
 
 const Socials = [
-    {
-        label: 'Facebook',
-        icon: <FaFacebook />,
-        link: '#',
-    },
-    {
-        label: 'Twitter',
-        icon: <FaXTwitter />,
-        link: '#',
-    },
-    {
-        label: 'Instagram',
-        icon: <FaInstagram />,
-        link: '#',
-    },
-    {
-        label: 'LinkedIn',
-        icon: <FaLinkedin />,
-        link: '#',
-    },
-    {
-        label: 'Youtube',
-        icon: <FaYoutube />,
-        link: '#',
-    },
-]
+  {
+    label: "Facebook",
+    icon: <FaFacebook />,
+    link: "#",
+  },
+  {
+    label: "Twitter",
+    icon: <FaXTwitter />,
+    link: "#",
+  },
+  {
+    label: "Instagram",
+    icon: <FaInstagram />,
+    link: "#",
+  },
+  {
+    label: "LinkedIn",
+    icon: <FaLinkedin />,
+    link: "#",
+  },
+  {
+    label: "Youtube",
+    icon: <FaYoutube />,
+    link: "#",
+  },
+];
 
 const Footer = () => {
-    return (
-        <footer className="p-14 flex flex-col w-full h-full relative bg-neutral-50 shadow-md border-t max-md:p-7">
-            {/* Upper part */}
-            <section className="flex items-center justify-between max-md:space-y-8 max-md:flex-col">
+  return (
+    <footer className="p-14 flex flex-col w-full h-full relative bg-neutral-50 shadow-md border-t max-md:p-7">
+      {/* Upper part */}
+      <section className="flex items-center justify-between max-md:space-y-8 max-md:flex-col">
+        {/* Logo */}
+        <div className="">
+          <Link to={"/"} className="flex items-center justify-center">
+            <img src={Logo} className="w-14" alt="Logo" />
+            <div className="space-y-1">
+              <h1 className="font-bold text-3xl">PreciSync</h1>
+              {/* <p className="text-xs tracking-widest font-semibold">Precision Hub Pvt. Ltd.</p> */}
+            </div>
+          </Link>
+        </div>
 
-                {/* Logo */}
-                <div className="">
-                    <Link to={'/'} className="flex items-center justify-center">
-                        <img src={Logo} className="w-14" alt="Logo" />
-                        <div className="space-y-1">
-                            <h1 className="font-bold text-3xl">PreciSync</h1>
-                            {/* <p className="text-xs tracking-widest font-semibold">Precision Hub Pvt. Ltd.</p> */}
-                        </div>
-                    </Link>
-                </div>
+        {/* Services */}
+        <div>
+          <ul className="flex items-center justify-between space-x-8 max-md:space-x-6 font-semibold max-md:text-sm">
+            {Services.map((service) => (
+              <Link to={service.link}>{service.label}</Link>
+            ))}
+          </ul>
+        </div>
 
-                {/* Services */}
-                <div>
-                    <ul className="flex items-center justify-between space-x-8 max-md:space-x-6 font-semibold max-md:text-sm">
-                        {Services.map(service => (
-                            <Link to={service.link}>
-                                {service.label}
-                            </Link>
-                        ))}
-                    </ul>
-                </div>
+        {/* Socials */}
+        <div>
+          <ul className="flex items-center justify-between space-x-6 max-md:space-x-10 text-2xl ">
+            {Socials.map((social) => (
+              <Link key={social.label} to={social.link}>
+                {social.icon}
+              </Link>
+            ))}
+          </ul>
+        </div>
+      </section>
 
-                {/* Socials */}
-                <div>
-                    <ul className="flex items-center justify-between space-x-6 max-md:space-x-10 text-2xl ">
-                        {Socials.map(social => (
-                            <Link key={social.label} to={social.link}>
-                                {social.icon}
-                            </Link>
-                        ))}
-                    </ul>
-                </div>
+      {/* seperation */}
+      <Separator className="my-8 bg-black" />
 
-            </section>
+      {/* Lower part */}
+      <section className="flex items-center justify-center text-sm font-semibold gap-10 max-md:gap-6 max-md:flex-col">
+        ©2023 PrecisionHub IT. All rights reserved.
+        <div className="flex items-center justify-between space-x-6 max-md:space-x-4 underline">
+          <Link to={"#"}>Privacy Policy</Link>
+          <Link to={"#"}>Terms & Conditions</Link>
+          <Link to={"#"}>Cookie Policy</Link>
+        </div>
+      </section>
+    </footer>
+  );
+};
 
-            {/* seperation */}
-            <Separator className="my-8 bg-black" />
-
-            {/* Lower part */}
-            <section className="flex items-center justify-center text-sm font-semibold gap-10 max-md:gap-6 max-md:flex-col">
-                ©2023 PrecisionHub IT. All rights reserved.
-                <div className="flex items-center justify-between space-x-6 max-md:space-x-4 underline">
-                    <Link to={'#'}>Privacy Policy</Link>
-                    <Link to={'#'}>Terms & Conditions</Link>
-                    <Link to={'#'}>Cookie Policy</Link>
-                </div>
-            </section>
-        </footer>
-    )
-}
-
-export default Footer
+export default Footer;
--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -7,7 +7,7 @@ import { Button } from './ui/button';
 
 const FormSchema = z.object({
     email: z.string(),
-  });
+});
 
 const Newsletter = () => {
 
